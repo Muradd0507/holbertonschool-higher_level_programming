6f2f839655e54ff489c57f856f796340ad5bff3d
Muradd0507 <muradqulizade75@gmail.com>
2024-11-18 17:00:08 -0500

    7_python

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+a=word[:2]
+b=word[-2:]
+print(f"First 3 letters: {a}")
+print(f"Last 2 letters: {b}")
+print(f"Middle word: {word[1:-1]}")
