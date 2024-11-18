2073dde1c15bd8027f5bfe1f867251018baa0ed5
Muradd0507 <muradqulizade75@gmail.com>
2024-11-18 21:04:06 +0400

    3_python

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 number = 3.14159
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-k=0
-kesir=int(number-int(number)*100)
-tam=int(number)
-netice=tam+kesir/100
-print(f"Float: {netice}")
+number = 3.14159
+print(f"Float: {number:.2f}")
+
