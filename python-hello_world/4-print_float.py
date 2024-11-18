031d6273424a0750ceb183bd637ac1b83d540db9
Muradd0507 <muradqulizade75@gmail.com>
2024-11-18 21:09:35 +0400

    3_python

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 number = 3.14159
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-number = 3.14159
+
+# Float değerini 2 ondalık basamağa yuvarlayarak yazdırma
 print(f"Float: {number:.2f}")
+
 
