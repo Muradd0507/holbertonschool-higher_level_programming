394fb670a8a2d36c3e52db251d8b34f9778d2836
Muradd0507 <muradqulizade75@mail.com>
2024-11-21 14:44:20 -0500

    project9_2

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+sjkbsdkvhi
+"""
+
+
+class Square:
+    """
+    dsvb.skdjsbj
+    """
+    try:
+        def __init__(self, size=0):
+            self.__size = size
+    except TypeError:
+        print("size must be an integer")
+    except ValueError:
+        print("size must be >= 0")
