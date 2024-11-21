fa04f34dc73f5b6ccb209c1de00bd0af8f394e71
Muradd0507 <muradqulizade75@gmail.com>
2024-11-21 18:56:13 +0400

    project9_0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,10 +2,13 @@
 """
 asvuhadhfdkushufhhsfe
 """
+
+
 class Square:
     """
     hfrehflrfrueeruig
     """
+
     def __init__(self, num):
-        self.id = num ** 2
+        self.num = num ** 2
         return self.id
