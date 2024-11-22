6cac49a6dc781df9ac5c100c166f034efcaf312f
Muradd0507 <muradqulizade75@gmail.com>
2024-11-22 19:23:53 +0400

    project10_1

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -8,10 +8,12 @@ class Rectangle:
     """
     hfewfhwer
     """
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        self.number_of_instances += 1
 
     @property
     def width(self):
@@ -57,3 +59,4 @@ class Rectangle:
 
     def __del__(self):
         print('Bye rectangle...')
+        self.number_of_instances -= 1
