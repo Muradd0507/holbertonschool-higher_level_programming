e92960612934ffc0f67c0908e581ef773d4fdaea
Muradd0507 <muradqulizade75@gmail.com>
2024-11-22 14:57:58 +0400

    project9_2

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+learning classes
+"""
+
+
+class Square:
+    """
+    raising errors
+    """
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        print("--")
