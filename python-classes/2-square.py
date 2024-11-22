91afebdf95e287fb28f9520ea9e54d174053436e
Muradd0507 <muradqulizade75@gmail.com>
2024-11-22 13:15:44 +0400

    project9_2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,9 +8,10 @@ class Square:
     """
     dsvb.skdjsbj
     """
+
     def __init__(self, size=0):
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size<0:
+        if size < 0:
             raise ValueError("size must be >= 0")
