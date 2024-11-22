89113558025a464276f0b4f5afdd12eacebe52e6
Muradd0507 <muradqulizade75@gmail.com>
2024-11-22 15:21:19 +0400

    project9_2

---
+++ python-classes/6-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+learning classes
+"""
+
+
+class Square:
+    """
+    raising errors
+    """
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if len(self.__position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
+                    print("#", end="")
+                print()
