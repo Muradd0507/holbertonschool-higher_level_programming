12c346e092f776148fd7223c8e73aadf21696c25
Muradd0507 <muradqulizade75@gmail.com>
2024-11-22 15:10:22 +0400

    project9_2

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -35,4 +35,4 @@ class Square:
         for i in range(0, self.__size):
             for j in range(0, self.__size):
                 print("#", end="")
-            print("\n")
+            print()
