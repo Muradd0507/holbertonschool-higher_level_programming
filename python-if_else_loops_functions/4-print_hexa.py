87ed9d12c5f87f597c393fe0b4bbf1d31a65cfbc
Muradd0507 <muradqulizade75@gmail.com>
2025-11-15 14:27:11 +0000

    aaa

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 result=''
-for i in range(0, 99):
-    result += "{} = 0x{:x}\n".format(i, i)
+    if (i == 98):
+        result += "{} = 0x{:x}".format(i, i)
+    else:
+        result += "{} = 0x{:x}\n".format(i, i)
 print(result)
