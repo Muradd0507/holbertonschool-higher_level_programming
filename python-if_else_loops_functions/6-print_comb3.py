6e627eacc6523f3472f61f23bf407e996f9089b5
Muradd0507 <muradqulizade75@gmail.com>
2025-11-16 10:50:39 +0000

    aaa

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(1, 10):
-        if ("{}{} ".format(i, j) == "{}{} ".format(j, i)):
+        if (("{}{} ".format(i, j) == "{}{} ".format(j, i)) or (i > j)):
             continue
         else:
             if (i == 8 and j == 9):
