9a1c0a9cbc8936c1e4b141289f6dfa5aa0b0d864
Muradd0507 <muradqulizade75@gmail.com>
2025-11-17 18:04:08 +0000

    aaa

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,6 +3,4 @@ def print_list_integer(my_list=[]):
     s=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in my_list:
         for j in s:
-            if i == j:
-                print("{:d}".format(i))
-                break
+            print("{:d}".format(i))
