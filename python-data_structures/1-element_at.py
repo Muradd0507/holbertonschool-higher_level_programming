805c30b36a980d0e1cfcd6d5bce3f7ae932e409b
Muradd0507 <muradqulizade75@gmail.com>
2025-11-18 17:03:10 +0000

    aaa

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@ def element_at(my_list, idx):
     for i in range(len(my_list)):
         if (i == idx):
             return my_list[i]
-        else:
-            return None
+    else:
+        return None
