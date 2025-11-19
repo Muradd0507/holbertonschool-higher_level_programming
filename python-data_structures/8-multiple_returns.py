7619a2332262fd60105f4c908561708c69d74c49
Muradd0507 <muradqulizade75@gmail.com>
2025-11-19 22:59:43 +0000

    aaa

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,9 +2,8 @@
 def multiple_returns(sentence):
     t = ()
     t = list(t)
-    a1 = len(sentence)
-    a2 = sentence[0]
-    t.append(a1)
-    t.append(a2)
-    t = tuple(t)
-    return t
+    if (len(sentence) == 0):
+        a2 = None
+    else:
+        a1 = len(sentence)
+        a2 = sentence[0]
