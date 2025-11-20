24920bae50d629b01dae5c85640d81476a10d347
Muradd0507 <muradqulizade75@gmail.com>
2025-11-20 20:28:56 +0000

    aaa

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for i, j in a_dictionary.items():
-        if (i == a_dictionary[key]):
-            a_dictionary[key] = value
+    if (key in a_dictionary.keys()):
+        for i, j in a_dictionary.items():
+            if (i == a_dictionary[key]):
+                a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
     return a_dictionary
