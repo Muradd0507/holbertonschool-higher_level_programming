fa45cb23e5deb76dc51980c0a1912928db4a123b
Muradd0507 <muradqulizade75@gmail.com>
2025-11-20 16:19:38 +0000

    aaa

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,7 @@ from functools import reduce
 def uniq_add(my_list=[]):
     s = set(my_list)
     s = list(s)
-    k = reduce(lambda a, b: a+b, s)
+    k = 0
+    for i in s:
+        k += i
     return k
