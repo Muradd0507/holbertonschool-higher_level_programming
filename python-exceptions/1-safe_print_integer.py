87a1c3f4cbc18345a51ca166b9c63ab0b66efb7a
Muradd0507 <muradqulizade75@gmail.com>
2025-11-22 10:10:39 +0000

    aaa

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     try:
         value = int(value)
-        print("{:d}".format(value))
-        return True
     except ValueError:
         return False
+    else:
+        value = int(value)
+        print("{:d}".format(value))
