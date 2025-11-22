1d0486792f7a0243e28ed6de08732490b5a2ceb4
Muradd0507 <muradqulizade75@gmail.com>
2025-11-22 10:28:48 +0000

    aaa

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
                 count += 1
-            except (ValueError, TypeError):
-                pass  # integer deyil → sadəcə keç
-    except IndexError:
-        pass  # x siyahıdan böyükdür → sakitcə dayandır
+        except (ValueError, TypeError):
+            pass  # integer deyil → sadəcə keç
     print()
     return count
