bbb5f9e87270af76a4cc0e6f5aa9536918125899
Muradd0507 <muradqulizade75@gmail.com>
2025-11-22 09:41:06 +0000

    aaa

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    k = 0
+    try:
+        for i in my_list:
+            if (k < x):
+                print("{:d}".format(i), end='')
+            k += 1
+        print()
+        return k
+
+    except IndentationError:
+        return None
