c8f0f79d4bae89dad91bc2f8873098dfb0824138
Muradd0507 <muradqulizade75@gmail.com>
2025-11-22 10:16:02 +0000

    aaa

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value = int(value)
+        test = value + 1
     except (ValueError, TypeError):
         return False
     else:
