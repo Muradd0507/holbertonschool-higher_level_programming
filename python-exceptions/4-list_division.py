bce07f07237581add59cbc09fda49bb375c01183
Muradd0507 <muradqulizade75@gmail.com>
2025-11-22 11:36:45 +0000

    aaa

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
             if (int(result) % result != 0):
-                raise ValueError("0"):
+                raise ValueError("0")
             s.append(result)
         except ValueError as e:
             s.append(int(e))
