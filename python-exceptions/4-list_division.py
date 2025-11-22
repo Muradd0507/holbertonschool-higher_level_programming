4a83460f2dbbae34e3292171c26999df04d78dd1
Muradd0507 <muradqulizade75@gmail.com>
2025-11-22 11:34:08 +0000

    aaa

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+def list_division(my_list_1, my_list_2, list_length):
+    s = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+            if (int(result) % result != 0):
+                raise ValueError():
+            s.append(result)
+        except ValueError as e:
+            s.append(0)
+        except (TypeError):
+            print("wrong type")
+        except (ZeroDivisionError):
+            print("division by 0")
+        except (IndexError):
+            print("out of range")
+    finally:
+        return s
