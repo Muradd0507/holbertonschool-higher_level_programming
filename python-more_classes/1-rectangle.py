cedae222361614128de26bbf09f1a993953c896d
Muradd0507 <muradqulizade75@gmail.com>
2025-11-24 21:08:10 +0000

    aaa

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,8 +10,6 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
         if (type(self.__width) is not int):
             raise TypeError("width must be an integer")
         if (type(self.__height) is not int):
@@ -20,6 +18,8 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         if (self.__height <= 0):
             raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
