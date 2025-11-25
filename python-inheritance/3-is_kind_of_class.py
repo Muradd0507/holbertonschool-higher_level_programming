00ab33cc6dd9d3711ab88fbfd17c4d4b8b41f476
Muradd0507 <muradqulizade75@gmail.com>
2025-11-25 12:39:11 +0000

    aaa

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a function that cheks isinstance
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks, if obj isinstance a_class returns true
+    """
+    if (isinstance(obj, a_class)):
+        return True
+    else:
+        return False
