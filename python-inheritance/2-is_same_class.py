90a44942d1ad6fe92242b6e7469d34b69ac946a4
Muradd0507 <muradqulizade75@gmail.com>
2025-11-25 12:36:15 +0000

    aaa

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,12 +3,11 @@
 Write a function that cheks isinstance
 """
 
-
 def is_same_class(obj, a_class):
     """
     Checks, if obj isinstance a_class returns true
     """
-    if (type(obj) == a_class):
+    if (type(obj) is a_class):
         return True
     else:
         return False
