aef02a0165cbb3cc068928ac2039b96f9d272583
Muradd0507 <muradqulizade75@gmail.com>
2025-11-26 12:23:25 +0000

    aaa

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
