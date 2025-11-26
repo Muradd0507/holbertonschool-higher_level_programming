544afc873fd9e3a28f6546489e99750e72141398
Muradd0507 <muradqulizade75@gmail.com>
2025-11-26 17:18:05 +0000

    aaa

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+appenddd
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appenddd
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        f.write(text)
+        return len(text
+    
