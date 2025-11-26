c1fbfa02f2eaf98a6bb95766540825b17a610206
Muradd0507 <muradqulizade75@gmail.com>
2025-11-26 20:16:57 +0000

    aaa

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+dbjkdbvdskv
+"""
+
+
+class Student:
+    """
+    sdbsdbvdb
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return lcs.__dict__
