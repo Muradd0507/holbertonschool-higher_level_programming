d5edc23dfad5f98eeda4166ebb527c122a8046eb
Muradd0507 <muradqulizade75@gmail.com>
2025-11-26 17:53:31 +0000

    aaa

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ def load_from_json_file(filename):
     """
     OBJ TO TEXT WITH JSON
     """
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
