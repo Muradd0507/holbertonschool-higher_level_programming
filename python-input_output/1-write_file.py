3909115ae4aad590910f91dbc5809b02073bfed4
Muradd0507 <muradqulizade75@gmail.com>
2025-11-26 17:10:16 +0000

    aaa

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+writee
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writee
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        print(f.write(text, end='')
