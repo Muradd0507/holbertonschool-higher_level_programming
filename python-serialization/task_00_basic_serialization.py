9b3d183b5fad068f43ab57247fb3483f8f13e0e2
Muradd0507 <muradqulizade75@gmail.com>
2025-11-27 09:09:28 -0500

    aaa

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,13 @@
+import json
+
+def serialize_and_save_to_file(data, filename):
+    # Your code here to serialize and save data to the specified file
+    with open(filename, 'w', encoding='utf-8') as f:
+        result = json.dump(data)
+        return result
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, 'w', encoding='utf-8') as f:
+        result = json.load(filename)
+        return result
