11203fdf651e4fd41e23a280560797d0cc3eb415
Muradd0507 <muradqulizade75@gmail.com>
2025-11-27 13:55:07 -0500

    aaa

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,25 @@
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, 'w') as f:
+            result = pickle.dumps(self)
+            f.write(result)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'r') as f:
+            result = pickle.load(f)
+        return result
