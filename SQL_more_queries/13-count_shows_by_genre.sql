f6a783289f7459a31b286c80ba458c5d61f7c3dc
Muradd0507 <muradqulizade75@gmail.com>
2025-11-29 22:15:14 +0000

    aaa

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_genres.name AS name , COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY number_of_shows; 
+ORDER BY number_of_shows DESC;
