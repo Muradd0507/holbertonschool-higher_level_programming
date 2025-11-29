4c99dfa76d3b199ad50e11f830a40d037ea9f26a
Muradd0507 <muradqulizade75@gmail.com>
2025-11-29 14:43:16 +0000

    aaa

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creating user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVELEGES TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVELEGES ON '.'TO 'user_0d_1'@'localhost';
