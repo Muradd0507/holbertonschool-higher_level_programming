5891ec9943d042e915464b4ce14da79cac4b19a4
Muradd0507 <muradqulizade75@gmail.com>
2025-11-29 16:36:31 +0000

    aaa

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL
+	FOREING KEY cities(state_id) REFERENCES TO states(id));
