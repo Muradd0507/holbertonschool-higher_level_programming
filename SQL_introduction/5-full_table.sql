501d781f1dbc4fe70713dff31ea939476858f737
Muradd0507 <muradqulizade75@gmail.com>
2025-11-29 01:36:29 +0000

    aaa

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,4 @@
 -- Create
 CREATE DATABASE hbtn_0c_0;
 USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table (
-        id INT,
-        name VARCHAR(256)
-);
-DESC first_table;
+SHOW CREATE TABLE first_table IF NOT EXISTS;
