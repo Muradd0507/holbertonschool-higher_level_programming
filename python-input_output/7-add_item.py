bca7299ce1cd4eb664ebeec3ac29a188536f9241
Muradd0507 <muradqulizade75@gmail.com>
2025-11-30 17:44:13 +0000

    aaa

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This
+is my docummentation
+"""
+
+import json
+import sys
+
+
+def load_from_json_file(filename):
+    """Just a doc 2"""
+    with open(filename, "r") as f:
+        data = f.read()
+        if not data:
+            return []
+        return json.loads(data)
+        f.close()
+
+
+def save_to_json_file(my_obj, filename):
+    """Just a doc"""
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
+
+
+file = open('add_item.json', 'a')
+file.close()
+
+currentdata = load_from_json_file('add_item.json')
+
+if len(sys.argv) > 1:
+    finaldata = currentdata + sys.argv[1:]
+    save_to_json_file(finaldata, 'add_item.json')
+else:
+    save_to_json_file(currentdata, 'add_item.json')
