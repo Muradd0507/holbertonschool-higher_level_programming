ba9fa4187232e72942fa729d5a1d03be67ceab6a
Muradd0507 <muradqulizade75@gmail.com>
2025-11-30 17:35:55 +0000

    aaa

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,3 +7,16 @@ pascal triade
 def pascal_triangle(n):
     if (n <= 0):
         return []
+
+    triangle = [[1]]  # İlk sıra
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        row = [1]  # Hər zaman 1 ilə başlayır
+        # Ortadakı elementləri hesabla
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)  # Hər zaman 1 ilə bitir
+        triangle.append(row)
+
+    return triangle
