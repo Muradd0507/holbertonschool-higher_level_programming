43f718630f2854dccfb004d02fb1195885741a95
Muradd0507 <muradqulizade75@gmail.com>
2025-11-30 17:58:46 +0000

    aaa

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,10 @@
---second table
-CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT);
-INSERT INTO second_table (id, name, score)
+-- ts is a comment
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table(id, name, score)
 VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
