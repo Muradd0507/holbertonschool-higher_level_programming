b1d1ffcff5ee021fc2fdf97f79a470bbecf1889d
Muradd0507 <muradqulizade75@gmail.com>
2025-12-01 17:12:32 +0000

    aaa

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import urllib
+"""
+This module is using for handling URLs
+"""
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
     u = url.read().urllib.request.decode('utf-8')
