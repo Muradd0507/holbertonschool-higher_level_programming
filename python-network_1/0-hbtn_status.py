3233226f76161127b71299a3cd300eb9b3c70d83
Muradd0507 <muradqulizade75@gmail.com>
2025-12-01 17:18:47 +0000

    aaa

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,10 +3,12 @@
 dssdf
 """
 import urllib.request
+import urllib.parse
 """
 This module is using for handling URLs
 """
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
+url = 'https://intranet.hbtn.io/status'
+req = request.Request(url, headers={'cfclearance': 'true'})
+with urllib.request.urlopen(req) as url:
     u = url.read().decode('utf-8')
     print(u)
