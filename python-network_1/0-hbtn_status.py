abbeb33726163091cd54244890dbe1391fe7e015
Muradd0507 <muradqulizade75@gmail.com>
2025-12-01 17:24:25 +0000

    aaa

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -10,4 +10,6 @@ url = 'https://intranet.hbtn.io/status'
 req = urllib.request.Request(url, headers={'cfclearance': 'true'})
 with urllib.request.urlopen(req) as url:
     u = url.read().decode('utf-8')
-    print(u)
+    print(f"\t- type: {type(u)}")
+    print(f"\t- content: {u}")
+    print(f"\t- utf8 content: {u.decode('utf-8')}")
