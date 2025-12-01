1e88f0a33ef02ea5d96c9c2caa7d8cf58c581997
Muradd0507 <muradqulizade75@gmail.com>
2025-12-01 17:25:16 +0000

    aaa

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ This module is using for handling URLs
 url = 'https://intranet.hbtn.io/status'
 req = urllib.request.Request(url, headers={'cfclearance': 'true'})
 with urllib.request.urlopen(req) as url:
-    u = url.read().decode('utf-8')
+    u = url.read()
     print(f"\t- type: {type(u)}")
     print(f"\t- content: {u}")
     print(f"\t- utf8 content: {u.decode('utf-8')}")
