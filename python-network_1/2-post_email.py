d297ed5ecfb37b83ba028f8792841be3f5537658
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 07:26:54 +0000

    aaa

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Sends a POST request with an email and prints the response body."""
+"""Sends a POST request with an email"""
 import urllib.request
 import urllib.parse
 import sys
