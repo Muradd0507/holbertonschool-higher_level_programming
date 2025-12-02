0d3022a826692457654b28379a203fac66c0c701
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 07:11:43 +0000

    aaa

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-import urllib
+"""
+POSTing email
+"""
+import urllib.request
+import urllib.parse
 import sys
-"""
-POSTing mail
-"""
 
-sys.argv = ['2-post_email.py', 'http://0.0.0.0:5000/post_email', 'hr@holbertonschool.com']
 url = sys.argv[1]
 email = sys.argv[2]
-data = {'post_email': email}
-data = urllib.parse.urlencode(data).encode('utf-8')
-req = urllib.request.Request(url, data=data)
 
-with urllib.request.urlopen(req) as u:
-    res = u.read().decode('utf-8')
-    print(res)
+# send as variable name "email"
+data = {"email": email}
+encoded = urllib.parse.urlencode(data).encode("utf-8")
+
+req = urllib.request.Request(url, data=encoded)
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode("utf-8")
+    print(body)
+
