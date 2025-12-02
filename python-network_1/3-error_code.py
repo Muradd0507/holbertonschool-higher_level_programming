6da99746ec0809425a22a6f6fe07c6957686b496
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 07:45:39 +0000

    aaa

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+try except with url example
+"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as u:
+            res = u.read().decode('utf-8')
+            print(res)
+    except urllib.error.HTTPError as e:
+        print("Error code: ", e.code)
