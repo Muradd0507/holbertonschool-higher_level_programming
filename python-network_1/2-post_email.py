1b581455ca02bf2dc9ea82b942a5818bb963486d
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 07:19:32 +0000

    aaa

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = {"email": email}
+    data = {"email": email, 'cfclearance': 'true'}
     encoded = urllib.parse.urlencode(data).encode("utf-8")
 
     req = urllib.request.Request(url, data=encoded)
