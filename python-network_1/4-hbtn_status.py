0cbddf480b1861fb1778ef3359f2ceed46460bf5
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 08:10:22 +0000

    aaa

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,11 +2,11 @@
 """
 URL fetching
 """
-import usrllib.request
+import requests
 
 url = 'https://intranet.hbtn.io/status'
 head = {'cfclearance': 'true'}
-req = request.Request(url, headers=head)
-with request.urlopen(req) as u:
-    res = u.read().decode('utf-8')
-    print(res)
+req = requests.get(url, headers=head)
+print("Body response:\n")
+print(f"\t- {type(req)}")
+print(f"\t- {req.text}")
