e0efb8f24588abed6b03d303993310bfb95565a6
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 07:06:28 +0000

    aaa

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -10,6 +10,7 @@ url = sys.argv[1]
 email = sys.argv[2]
 data = {'post_email': email}
 data = urllib.parse.urlencode(data).encode('utf-8')
+req = urllib.request.Request(url, data=data)
 
-with urllib.request.urlopen(url, data=data) as u:
+with urllib.request.urlopen(req) as u:
     res = u.read().decode()
