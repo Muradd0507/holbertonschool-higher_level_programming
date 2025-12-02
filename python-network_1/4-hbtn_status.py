f64c65a5f5c395cd643580b61cf4b7f7229cf1c5
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 08:01:43 +0000

    aaa

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,7 +5,8 @@ URL fetching
 import usrllib.request
 
 url = 'https://intranet.hbtn.io/status'
-
-with request.urlopen(url) as u:
+head = {'cfclearance': 'true'}
+req = request.Request(url, headers=head)
+with request.urlopen(req) as u:
     res = u.read().decode('utf-8')
     print(res)
