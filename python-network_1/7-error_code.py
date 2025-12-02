ce38ab8e3fa98f82b17244de721f37ea4e82647d
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 14:12:30 +0000

    aaa

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     res = requests.get(url)
-    if (res.reason >= 400):
-        print(f'Error code: {res.reason}')
+    if (res.status_code >= 400):
+        print(f'Error code: {status_code}')
     else:
         print(res.text)
