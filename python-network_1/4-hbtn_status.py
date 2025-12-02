a66037ff1035aea92f848d72e56339dbe45fbd2c
Muradd0507 <muradqulizade75@gmail.com>
2025-12-02 08:11:38 +0000

    aaa

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ url = 'https://intranet.hbtn.io/status'
 head = {'cfclearance': 'true'}
 req = requests.get(url, headers=head)
 print("Body response:\n")
-print(f"\t- {type(req)}")
-print(f"\t- {req.text}")
+print(f"\t- {type(req.text)}")
+print(f"\t- content :{req.text}")
