e2ef75f49b50846d3968187ea2342bce34a6ff5b
Muradd0507 <muradqulizade75@gmail.com>
2025-12-03 02:18:19 -0500

    aaa

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -2,12 +2,27 @@
 import requests
 import csv
 
+# 1. Fetch və ekranda göstərmək
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
+
     print("Status Code:", response.status_code)
 
-    if (response.status_code == 200):
+    if response.status_code == 200:
+        data = response.json()   # JSON → Python list
+        for post in data:
+            print(post["title"])  # hər postun title-i
+    else:
+        print("Request failed.")
+
+
+# 2. Fetch və CSV-ə yazmaq
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
         data = response.json()
 
         posts_list = []
@@ -18,6 +33,7 @@ def fetch_and_print_posts():
                 "body": post["body"]
             })
 
+        # CSV-ə yazmaq
         with open("posts.csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
             writer.writeheader()
