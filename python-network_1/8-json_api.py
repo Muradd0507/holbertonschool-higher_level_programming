b465c1655bbcfb0c87461bf3c4e1958624addbe7
Muradd0507 <muradqulizade75@gmail.com>
2025-12-06 22:49:25 +0000

    aaa;'fe;'flw;lfkwlkfwekfo

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+"""
+json api
+"""
+import requests
+import sys
+
+
+url = 'http://0.0.0.0:5000/search_user'
+try:
+    q = sys.argv[1]
+except:
+    q = ""
+res = requests.post(url, data=q, method='POST')
+
+try:
+    json_data = res.json()
+    if json_data == {} or json_data == []:
+        print("No result")
+    else:
+        print("{} {}".format(json_data['id'], json_data['name']))
+
+except requests.exceptions.JSONDecodeError:
+    print("Data is not json")
+
+
