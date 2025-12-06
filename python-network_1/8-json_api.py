82b3b097fa8e9512f53bc305a8836f2ba48374b8
Muradd0507 <muradqulizade75@gmail.com>
2025-12-06 23:19:21 +0000

    aaa

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -2,9 +2,10 @@
 import requests
 import sys
 
+header = {'cfclearance': 'true'}
 q = sys.argv[1] if len(sys.argv) > 1 else ""
 try:
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q}, headers=header)
     j = r.json()
     if not j:
         print("No result")
